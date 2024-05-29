a86906af88e3baffb87279f9d86ca7850ce44a86
Maria Eduarda Barone <duda.barone@outlook.com>
2024-05-29 11:20:39 +0200

    ex001

---
+++ Introduction/Variables/ex001/src/Start.java
@@ -0,0 +1,8 @@
+public class Start {
+    public static void main(String[] args) {
+    int apples = 3;
+    int oranges = 4;
+    System.out.println("Apples: "+apples);
+    System.out.println("Oranges: " +oranges);
+}
+}
