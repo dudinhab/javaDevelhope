6f0939def44daa7fbc8761978afc6157107a12c9
Maria Eduarda Barone <duda.barone@outlook.com>
2024-05-29 16:14:06 +0200

    Hello World

---
+++ Introduction/helloWorld/src/Start.java
@@ -0,0 +1,6 @@
+public class Start {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Hello develhope java student!");
+    }
+}
